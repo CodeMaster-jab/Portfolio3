6460e82fefbb4fb32d85ab3d184f0768fd6759ed
Code Master <jeffbell.kc@gmail.com>
2020-04-24 18:30:32 -0500

    Fixed bugs and styling issues

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,14 +4,14 @@ import "./style.css";
 function Project(props) {
   return (
     <div className="col-lg-4 col-sm-12">
-      <div className="card" style={{width: '23rem'}}>
+      <div className="card mt-1" style={{fontSize: '12pt', width: '23rem'}}>
         <img src={props.imgSrc} alt={props.title} width="50%" />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{props.authors}</h6>
           <p className="card-text">{props.description}</p>
-          <a href={props.appLink} target="_blank" className="card-link">Application</a>
-          <a href={props.repoLink} target="_blank" className="card-link">GitHub</a>
+          <a href={props.appLink} target="_blank" rel="noopener noreferrer" className="card-link">Application</a>
+          <a href={props.repoLink} target="_blank" rel="noopener noreferrer" className="card-link">GitHub</a>
         </div>
       </div>
     </div>
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -13,7 +13,7 @@ function Portfolio() {
         </div>
         <div className="row mt-2">
           <div className="col-12 col-md-4">
-            <img className='myPicture img-fluid' src="./images/MeBenji.jpg"/>
+            <img className='myPicture img-fluid' src="./images/MeBenji.jpg" alt=""/>
           </div>
           <div className="col-12 col-md-8 pt-2 pt-md-0">
             <p>I currently live and work in Overland Park Kansas.  I graduated from DeVry University in October 1989, and took a Job with Sprint in Janurary 1990.  I have been developing software for various platforms using many different technologies and languages for over thirty years.</p>
@@ -39,9 +39,9 @@ function Portfolio() {
           <div className="col-md-6">
             <p>Links:</p>
             <ul id="Links">
-              <li><a href="https://drive.google.com/file/d/1zu9NM_kB6YrpX1rEqLtzEz8f4rvMbg2S/view?usp=sharing" target="_blank">Resume</a></li>
-              <li><a href="https://github.com/CodeMaster-jab" target="_blank">GitHub Profile</a></li>
-              <li><a href="https://www.linkedin.com/in/jeff-bell-573a4711/" target="_blank">LinkedIn Profile</a></li>
+              <li><a href="https://drive.google.com/file/d/1zu9NM_kB6YrpX1rEqLtzEz8f4rvMbg2S/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
+              <li><a href="https://github.com/CodeMaster-jab" target="_blank"rel="noopener noreferrer" >GitHub Profile</a></li>
+              <li><a href="https://www.linkedin.com/in/jeff-bell-573a4711/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></li>
             </ul>
           </div>
         </div>
